3d2215d0f23cdc9564b266c0aae78cf074a88d7c
Kundadanti Jalindar Kurhe <105533977+KundadantiKurhe@users.noreply.github.com>
2024-03-14 21:26:44 +0530

    Update CountOccurenacesofCharacterString.java

--- Basic_programs/src/SDET_QA/CountOccurenacesofCharacterString.java
+++ Basic_programs/src/SDET_QA/CountOccurenacesofCharacterString.java
@@ -4,8 +4,9 @@ public class CountOccurenacesofCharacterString {
 
 	public static void main(String[] args) {
 		String  s = "Java Programing Java oops";
+		String s1 = s.toLowerCase();
 		int totalCount = s.length();  // total len
-		int totalCount_afterremoves = s.replace("a" , "").length(); //total len after removing a's
+		int totalCount_afterremoves = s1.replace("a" , "").length(); //total len after removing a's
 		int count = totalCount - totalCount_afterremoves;
 		System.out.println("Number occuarnce of a is : " + count);
 			
