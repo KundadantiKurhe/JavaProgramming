d350a78aa347a94f240e8ecf902db69f1f37de57
Kundadanti Jalindar Kurhe <105533977+KundadantiKurhe@users.noreply.github.com>
2024-03-16 12:49:46 +0530

    Update CountEvenOddNumber.java
    Handle Even- odd count in integer values by using Map Collection and logic handled in the method.

--- Basic_programs/src/SDET_QA/CountEvenOddNumber.java
+++ Basic_programs/src/SDET_QA/CountEvenOddNumber.java
@@ -1,7 +1,7 @@
 package SDET_QA;
 
 import java.util.Scanner;
-
+// Approach 1
 public class CountEvenOddNumber {
 
 	public static void main(String[] args) {
@@ -28,5 +28,43 @@ public class CountEvenOddNumber {
 			System.out.println("count of even number" + evenCount);
 			System.out.println("count of odd number" + oddCount);
 
+	}
+}
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+// Approach 2
+public class EvenOddnumber {
+	public static void main(String[] args) {
+		System.out.println("Enter integer Value..");
+		try (Scanner sc = new Scanner(System.in)) {
+			int num = sc.nextInt();
+			System.out.println("num : "+num);
+			
+			Map<String, Integer> oddEvenCountValuesMap = checkOddEvenCountInGivenNumber(num);
+			System.out.println("oddEvenCountValuesMap : "+ oddEvenCountValuesMap);
+		}
+	}
+	
+	public static Map<String, Integer> checkOddEvenCountInGivenNumber(int num){
+		int evenCount = 0;
+		int oddCount = 0;
+		while(num>0){
+			int rem  = num%10;
+			System.out.println("rem : "+rem);
+			if(rem%2 == 0)
+				evenCount++;
+			else
+				oddCount++;
+			num= num/10;
+			System.out.println("num 2 : "+num);
+		}
+		
+		Map<String, Integer> countValues = new HashMap<>();
+		countValues.put("evenCount", evenCount);
+		countValues.put("oddCount", oddCount);
+		return countValues;
 	}
 }
