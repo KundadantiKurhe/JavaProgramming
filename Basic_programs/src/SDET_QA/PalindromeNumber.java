511685a8b1a6648563689ed1cab59b2880c8c8bf
Kundadanti Jalindar Kurhe <105533977+KundadantiKurhe@users.noreply.github.com>
2024-03-16 12:45:18 +0530

    Update PalindromeNumber.java
    Added new Two classes for Palindrome numbers by handling in the method approach.
    1. Reteun boolean value
    2. Return integer value

--- Basic_programs/src/SDET_QA/PalindromeNumber.java
+++ Basic_programs/src/SDET_QA/PalindromeNumber.java
@@ -2,27 +2,73 @@ package SDET_QA;
 
 import java.util.Scanner;
 
+// Approach 1
 public class PalindromeNumber {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("enter the number ");
-		
 		int num = sc.nextInt();
 		int org_num = num;
 		int rev  = 0; 
-		while(num!=0)
-		{
+		while(num!=0){
 			rev = rev*10+num%10;
 			num = num/10;
 		}
 		
-		if(org_num == rev) {
+		if(org_num == rev) 
 			System.out.println("Palindrome..." + rev);
-		}
 		else
-		{
 			System.out.println("Not a Palindrome..." + rev);
+	}
+}
+
+// Approach 2
+//by returnning boolean values
+
+public class PalindromeNumber {
+	public static void main(String[] args) {
+		int n = 121;
+		if( paliNumber(n))
+			System.out.println(n + " : Palindrome number");
+		else
+			System.out.println(n + " : not palindrome number");
+	}
+	
+	public static boolean paliNumber(int n) {
+		int temp=n;
+		int rev=0, rem ;
+		while(n !=0) {
+			 rem = n%10;
+			 rev = rev *10+rem;
+			n=n/10;
 		}
+		return temp == rev;
+	}
+}
+
+//Approach 3
+//by returnning integer values
+
+public class PalindromeNumber {
+	public static void main(String[] args) {
+		int n = 121;
+		
+		int rs = paliNumber(n);
+		if(rs == n)
+			System.out.println("Palindrome number");
+		else 
+			System.out.println("not palindrome number");
+	}
+	
+	public static int paliNumber(int n) {
+		int temp=n;
+		int rev=0, rem ;
+		while(temp !=0) {
+			 rem = temp%10;
+			 rev = rev *10+rem;
+			temp=temp/10;
+		}
+		return rev;
 	}
 }
